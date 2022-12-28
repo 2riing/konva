a870b9e63bdd6c818a46e3f0e3331c35fda8e1ea
2riing <5032303000@naver.com>
2022-12-28 18:01:31 +0900

    test: Import Konda and draw sample Rect

--- drawing-pjt/src/App.jsx
+++ drawing-pjt/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import Konva from "konva";
+import { render } from "react-dom";
+import { Stage, Layer, Rect, Text } from "react-konva";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const myClickEvent = () => {
+    console.log("click!");
+  };
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text="mytext test" />
+          <Rect
+            x={100}
+            y={100}
+            width={500}
+            height={500}
+            fill="red"
+            shadowBlur={5}
+            onClick={myClickEvent}
+          />
+        </Layer>
+      </Stage>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
